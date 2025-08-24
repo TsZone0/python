5f4534e530c79eaa135dd9864777757ea0dd23de
Shivam Kumar <shivamkumar.tit@gmail.com>
2025-08-24 07:48:22 +0530

    Add files via upload

---
+++ 01_basic/basics_of_py.py
@@ -0,0 +1,17 @@
+name = "shivam"
+age = 20
+price = 23.99
+
+age = 22
+
+
+# for printing
+print("my name is ", name)
+
+#for know the type
+print(type(age))
+
+num1 = 5
+num2 = 10
+sum = num1+num2
+print(sum)
